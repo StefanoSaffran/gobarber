1a6555961b21e5c253525defd480f8ccc7911ad9
Stefano Saffran <stefanosaffran@hotmail.com>
2020-05-15 17:19:06 +0200

    feat: add ListProviderAppointments feature

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
