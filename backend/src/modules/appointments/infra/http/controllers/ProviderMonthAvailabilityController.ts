b20c3761c0fd52582b6b9aac0f0349009f0ce853
Stefano Saffran <stefanosaffran@hotmail.com>
2020-05-15 17:28:30 +0200

    feat: new controller for provider month availability

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+
+export default class ProvidersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { month, year } = request.body;
+
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
+
+    const providers = await listProviderMonthAvailability.execute({
+      provider_id,
+      month,
+      year,
+    });
+
+    return response.json(providers);
+  }
+}
