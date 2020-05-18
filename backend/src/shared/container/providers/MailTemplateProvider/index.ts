de1cd8b46cedf07af9d7555a793306ab246560ae
Stefano Saffran <stefanosaffran@hotmail.com>
2020-05-18 12:19:43 +0200

    chore: organize the providers to handle the app growth easily

---
+++ backend/src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const providers = {
+  handlebars: HandlebarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  providers.handlebars,
+);
