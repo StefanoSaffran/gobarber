83e8f55844497e8cff27b29d659f7e800e439fe6
Stefano Saffran <stefanosaffran@hotmail.com>
2020-05-28 15:13:26 +0200

    feat: add new routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,9 @@ import { Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+
 import Dashboard from '../pages/Dashboard';
 
 import Route from './Route';
@@ -11,6 +14,8 @@ const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
