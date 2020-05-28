442d8da1a931c77fe4affdef1280e9abb3930a52
Stefano Saffran <stefanosaffran@hotmail.com>
2020-05-28 15:14:20 +0200

    feat: add implementation of the dashboard's Header

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FiPower } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://skylab.rocketseat.com.br/api/users/avatar/profile-30a7c284-0d48-4acb-a095-09b145d3d2a8.jpg"
+              alt="Stefano Saffran"
+            />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
