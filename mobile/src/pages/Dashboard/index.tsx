93419e2a88de9477c31c5e3461196b5373493801
Stefano Saffran <stefanosaffran@hotmail.com>
2020-06-16 16:19:11 +0200

    feat: finishing dashboard page

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,121 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
+import { View } from 'react-native';
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
+import * as S from './styles';
+
+export interface IProvider {
+  id: string;
+  name: string;
+  avatar_url: string;
+  nameInitials?: string;
+}
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<IProvider[]>([]);
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    // navigate('Profile');
+    signOut();
+  }, [signOut]);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', { providerId });
+    },
+    [navigate],
+  );
+
+  const nameInitials = useMemo(() => {
+    return user.name
+      .split(' ')
+      .map(name => name.charAt(0).toUpperCase())
+      .join('')
+      .substring(0, 2);
+  }, [user.name]);
+
+  useEffect(() => {
+    api.get<IProvider[]>('providers').then(response => {
+      setProviders(
+        response.data.map(provider =>
+          provider.avatar_url
+            ? provider
+            : {
+                ...provider,
+                nameInitials: provider.name
+                  .split(' ')
+                  .map(name => name.charAt(0).toUpperCase())
+                  .join('')
+                  .substring(0, 2),
+              },
+        ),
+      );
+    });
+  }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <S.Container>
+      <S.Header>
+        <S.HeaderTitle>
+          Bem vindo, {'\n'}
+          <S.UserName>{user.name}</S.UserName>
+        </S.HeaderTitle>
+
+        <S.ProfileButton onPress={navigateToProfile}>
+          {user.avatar_url ? (
+            <S.UserAvatar source={{ uri: user.avatar_url }} />
+          ) : (
+            <S.UserInitialsContainer>
+              <S.UserInitials>{nameInitials}</S.UserInitials>
+            </S.UserInitialsContainer>
+          )}
+        </S.ProfileButton>
+      </S.Header>
+
+      <S.ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        ListFooterComponent={<View style={{ marginBottom: 32 }} />}
+        ListHeaderComponent={
+          <S.ProvidersListTitle>Cabeleireiros</S.ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <S.ProviderContainer
+            onPress={() => {
+              navigateToCreateAppointment(provider.id);
+            }}
+          >
+            {provider.avatar_url ? (
+              <S.ProviderAvatar source={{ uri: provider.avatar_url }} />
+            ) : (
+              <S.ProviderInitialsContainer>
+                <S.ProviderInitials>{provider.nameInitials}</S.ProviderInitials>
+              </S.ProviderInitialsContainer>
+            )}
+
+            <S.ProviderInfo>
+              <S.ProviderName>{provider.name}</S.ProviderName>
+
+              <S.ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <S.ProviderMetaText>segunda à sexta</S.ProviderMetaText>
+              </S.ProviderMeta>
+
+              <S.ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <S.ProviderMetaText>8h às 18h</S.ProviderMetaText>
+              </S.ProviderMeta>
+            </S.ProviderInfo>
+          </S.ProviderContainer>
+        )}
+      />
+    </S.Container>
   );
 };
 
---
+++ mobile/src/pages/Dashboard/styles.ts
@@ -0,0 +1,129 @@
+import styled from 'styled-components/native';
+import { Platform, FlatList } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
+import { Provider } from './index';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${Platform.OS === 'ios'
+    ? getStatusBarHeight() + 24
+    : getStatusBarHeight()}px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Regular';
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  border-width: 1px;
+  border-color: #ff9000;
+  padding: 3px;
+`;
+
+export const UserInitialsContainer = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  background: #312e38;
+  color: #ff9000;
+  border: 1px #ff9000;
+  padding: 3px;
+`;
+export const UserInitials = styled.Text`
+  color: #ff9000;
+  font-size: 24px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ProvidersListTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  margin-bottom: 24px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProviderContainer = styled(RectButton)`
+  background: #3e3b47;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+  border-width: 1px;
+  border-color: #ff9000;
+  padding: 3px;
+`;
+
+export const ProviderInitialsContainer = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+  background: #312e38;
+  border: 1px #ff9000;
+  padding: 3px;
+`;
+export const ProviderInitials = styled.Text`
+  color: #ff9000;
+  font-size: 24px;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #f4ede8;
+`;
+
+export const ProviderMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+  margin-left: 8px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+`;
