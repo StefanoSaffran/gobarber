d0317e91234ddb188470f90accc596a59d6370d1
Stefano Saffran <stefanosaffran@hotmail.com>
2020-06-18 19:41:34 +0200

    feat: add AppointmentCreated page

---
+++ mobile/src/pages/AppointmentCreated/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  font-family: 'RobotoSlab-Medium';
+  color: #f4ede8;
+  margin-top: 48px;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+  color: #999591;
+  margin-top: 16px;
+`;
+
+export const OkButton = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+  background: #ff9000;
+  border-radius: 10px;
+  margin-top: 24px;
+  padding: 12px 24px;
+`;
+
+export const OkButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #312e38;
+`;
